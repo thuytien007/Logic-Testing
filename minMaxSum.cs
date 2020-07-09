7eddff010e3a22c54b5823a870f1fc33c4a84a59
Tien Nguyen Ngoc Thuy <Tien.Nguyen@SCCVietnam.local>
2020-07-09 10:48:08 +0100

    logic testing for 2 days intern

---
+++ minMaxSum.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Test
+{
+    class minMaxSum
+    {
+        //interchange sort
+        static void increasingSort(long[] arr)
+        {
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (arr[i] > arr[j])
+            //        {
+            //            //swap
+            //            long temp = arr[i];
+            //            arr[i] = arr[j];
+            //            arr[j] = temp;
+            //        }
+            //    }
+            //}
+            //flag value
+            //bool valuesSwitched = false;
+            //for (int i = 1; i <= arr.Length; i++)
+            //{
+                //if (i == arr.Length)
+                //{
+                    //if (!valuesSwitched) break;
+
+                    //valuesSwitched = false;
+                    //i = 1;
+                //}
+                //if (arr[i - 1] > arr[i])
+                //{
+                    //long temp = arr[i - 1];
+                    //arr[i - 1] = arr[i];
+                    //arr[i] = temp;
+                    //valuesSwitched = true;
+                //}
+            //}
+        //}
+        static void miniMaxSum(long[] arr)
+        {
+            //long min = 0;
+            //long max = 0;
+            //increasingSort(arr);
+            //solution 1 with 4 loops
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    min += arr[i];
+            //}
+            //for (int j = arr.Length-1; j > arr.Length-5; j--)
+            //{
+            //    max += arr[j];
+            //}
+            //solution 2 with 2 loops
+            /*long i = 0;
+            long lenght = arr.Length;
+            while(i <= lenght+1)
+            {
+                if (i < 4)
+                {
+                    min += arr[i];
+                    i++;
+                }   
+                if(lenght > arr.Length - 5)
+                {
+                    max += arr[lenght-1];
+                    lenght--;
+                }
+            }*/
+            //cach 3 voi 1 vong lap for
+            long min = arr[0];
+            long max = arr[0];
+            long sum = arr[0];
+            long v = 0;
+    
+            for (long i = 1; i < arr.Length; i++)
+            {
+                v = arr[i];
+                sum += v;
+                if (v > max) max = v;
+                if (v < min) min = v;
+            }
+            Console.WriteLine(sum - max);
+            Console.WriteLine(sum - min);
+        }
+        static void Main(string[] args)
+        {
+            long[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt64(arrTemp));
+            miniMaxSum(arr);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ pickingNumberTest2.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Result
+{
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<int> a)
+    {
+        a.Sort();
+	//with 2 for loop
+        //int len = a.Count;
+        //int max = 0;
+        //for (int i = len - 1; i >= 0; i--)
+        //{
+        //    int count = 1;
+        //    for (int j = i - 1; j >= 0; j--)
+        //    {
+
+        //        if ((a[i] - a[j]) < 2)
+        //        {
+        //            count++;
+        //        }
+        //    }
+        //    max = count > max ? count : max;
+        //}
+        //return max;
+
+	//with 1 for loop
+        int result = 0;
+        int count = 1;
+        int sub = a[0];
+
+        for (int i = 1; i < a.Count(); i++)
+        {
+            if (sub == a[i] || sub + 1 == a[i])
+            {
+                count++;
+            }
+            else
+            {
+                if (count > result)
+                {
+                    result = count;
+                }
+                count = 1;
+                sub = a[i];
+            }
+        }
+
+        if (count > result)
+        {
+            result = count;
+        }
+        return result;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        int result = Result.pickingNumbers(a);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
