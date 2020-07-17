c9534c330a676b966d4d97c021f5f51e914e11e8
Tien Nguyen Ngoc Thuy <Tien.Nguyen@SCCVietnam.local>
2020-07-17 12:20:50 +0100

    Mid Test after 2 weeks training

---
+++ MidTest/MidTest/Scripts/PersonViewModel.js
@@ -0,0 +1,135 @@
+﻿var userSample = [
+    { firstName: 'Lynnia', lastName: 'Eady' },
+    { firstName: 'Melaina', lastName: 'Beamon' },
+    { firstName: 'Anya', lastName: 'Weldon' },
+    { firstName: 'Leslie', lastName: 'Dickinson' },
+    { firstName: 'Ellissa', lastName: 'Peck' },
+    { firstName: 'Kaydince', lastName: 'Wallace' },
+    { firstName: 'Mracelynn', lastName: 'Baxter' },
+    { firstName: 'Kadince', lastName: 'Napier' },
+    { firstName: 'Desiree', lastName: 'Evans' },
+    { firstName: 'Syniah', lastName: 'Yount' },
+    { firstName: 'Madelina', lastName: 'Raines' },
+    { firstName: 'Dejai', lastName: 'Shute' },
+    { firstName: 'Toryn', lastName: 'Kirby' },
+    { firstName: 'Lyllian', lastName: 'Yost' },
+    { firstName: 'Emilie', lastName: 'Allison' },
+    { firstName: 'Emilia', lastName: 'Mcintyre' },
+    { firstName: 'Suhaib', lastName: 'Glenn' },
+    { firstName: 'Fanny', lastName: 'Cronan' },
+    { firstName: 'Bo', lastName: 'Abston' },
+    { firstName: 'Kriti', lastName: 'Rutledge' },
+    { firstName: 'Georgia', lastName: 'Tamayo' },
+    { firstName: 'Alivia', lastName: 'Higgins' },
+    { firstName: 'Yamilet', lastName: 'Abernathy' },
+    { firstName: 'Lanie', lastName: 'Maxwell' },
+    { firstName: 'Araceli', lastName: 'Bravo' },
+    { firstName: 'Astoria', lastName: 'Weyant' },
+];
+
+ko.bindingHandlers.clickAndStop = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel, context) {
+        var handler = ko.utils.unwrapObservable(valueAccessor()),
+            newValueAccessor = function () {
+                return function (data, event) {
+                    handler.call(viewModel, data, event);
+                    event.cancelBubble = true;
+                    if (event.stopPropagation) event.stopPropagation();
+                };
+            };
+
+        ko.bindingHandlers.click.init(element, newValueAccessor, allBindingsAccessor, viewModel, context);
+    }
+};
+
+//create an Object with fname, lname, age
+function PersonModel(firstName, lastName, age) {
+    this.firstName = ko.observable(firstName);
+    this.lastName = ko.observable(lastName);
+    this.age = ko.observable(age);
+};
+
+//loop array object, if pass condition then add to tempList
+function selectedUserList(oriArray) {
+    this.tempList = [];
+    oriArray.forEach(function (item) { 
+        if (item.firstName.length + item.lastName.length > 12 && item.firstName.charAt(0) == "M") {
+            var age = Math.floor(Math.random() * 99) + 1;
+            var user = new PersonModel(item.firstName, item.lastName, age);
+            this.tempList.push(user);
+        }
+    })
+    return tempList;
+}
+
+//sum age with selected user
+function sumAgeWithSelectedUser(arrObject) {
+    var sumAge = 0;
+    arrObject.forEach(function (item) {
+        sumAge += item.age();
+    })
+    return sumAge;
+}
+
+//function changeDataArray(personList, removedList, currentRowObject) {
+//    personList.remove(currentRowObject);
+//    removedList.push(currentRowObject);
+//}
+
+//main ViewModel
+function PersonViewModel() {
+    var self = this;
+
+    //list of all person
+    self.allPerson = ko.observableArray(userSample);
+
+    //list of selected person with condition
+    self.selectedPerson = ko.observableArray(selectedUserList(userSample));
+
+    //sum all age of selected person
+    self.sumAge = ko.computed(function () {
+        return sumAgeWithSelectedUser(self.selectedPerson());
+    });
+
+    //initial object person empty
+    self.objectSelectedRow = ko.observable(new PersonModel("","",0));
+    //select row click by user
+    self.selectedUser = function (item) {
+        self.objectSelectedRow(item);
+    }
+
+    //get age range
+    self.agePeriod = ko.computed(function () {
+        var status = " ";
+        if (self.objectSelectedRow().age() < 15)
+            status = "Teenager";
+        if (self.objectSelectedRow().age() > 62)
+            status = "Retired";
+        else
+            status = "undefine age range";
+        return status;
+    })
+
+    //get fullname
+    self.fullName = ko.computed(function(){
+        return self.objectSelectedRow().firstName() + " " + self.objectSelectedRow().lastName();
+    });
+
+    self.removedPersonList = ko.observableArray();
+    self.sumAgeRemovedPersonList = ko.computed(function () {
+        return sumAgeWithSelectedUser(self.removedPersonList());
+    });
+
+    self.removePerson = function () {
+        self.selectedPerson.remove(self.objectSelectedRow());
+        self.removedPersonList.push(self.objectSelectedRow());
+        self.sumAge - self.objectSelectedRow().age();
+    };
+
+    self.undoPerson = function () {
+        self.removedPersonList.remove(self.objectSelectedRow());
+        self.selectedPerson.push(self.objectSelectedRow());    
+    }
+}
+
+ko.applyBindings(new PersonViewModel());
---
+++ threeSubArrayTest3.cs
@@ -0,0 +1,239 @@
+﻿using System;
+
+//1 loop
+using System;
+
+namespace threeSubArrayTest3
+{
+    class threeSubArrayTest3
+    {
+        static int sumArray(int[] a)
+        {
+            int sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+        static int findEqualMaxTotal(int[] h1, int[] h2, int[] h3)
+        {
+            // your code goes here
+            int h1Sum = sumArray(h1);
+            int h2Sum = sumArray(h2);
+            int h3Sum = sumArray(h3);
+            int min = h1Sum;
+            if (h1Sum == h2Sum && h1Sum == h3Sum)
+            {
+                min = h1Sum;
+                return min;
+            }
+            //find min
+            if (h2Sum < min)
+                min = h2Sum;
+            if (h3Sum < min)
+                min = h3Sum;
+            int len = h1.Length + h2.Length + h3.Length;
+            int h1FE = h1[0], h2FE = h2[0], h3FE = h3[0];
+            bool flag = false;
+            for(int i = 1; i < len; i++)
+            {
+                if (i < h1.Length)
+                {
+                    if (h1Sum - h1FE <= min)
+                        { min = h1Sum - h1FE;}    
+                    else
+                        {h1FE += h1[i];}
+                }
+                if (i < h2.Length)
+                {
+                    if (h2Sum - h2FE <= min)
+                        { min = h2Sum - h2FE;}
+                    else
+                        { h2FE += h2[i];}
+                }
+                if(i < h3.Length)
+                {
+                    if (h3Sum - h3FE <= min)
+                        { min = h3Sum - h3FE;}
+                    else
+                        { h3FE += h3[i];}
+                }
+                if((h1Sum - h1FE == min) && (h2Sum - h2FE == min) && (h3Sum - h3FE == min))
+                {
+                    flag = true;
+                    return min;
+                }
+            }
+            if (flag == false)
+            {
+                min = 0;
+                return min;
+            }        
+            return min;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] h1 = Array.ConvertAll(Console.ReadLine().Split(' '), h1Temp => Convert.ToInt32(h1Temp));
+
+            int[] h2 = Array.ConvertAll(Console.ReadLine().Split(' '), h2Temp => Convert.ToInt32(h2Temp));
+
+            int[] h3 = Array.ConvertAll(Console.ReadLine().Split(' '), h3Temp => Convert.ToInt32(h3Temp));
+
+            int result = findEqualMaxTotal(h1, h2, h3);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
+
+//3 loops
+namespace threeSubArrayTest3
+{
+    class threeSubArrayTest3
+    {
+        static int sumArray(int[] a, int startPosition)
+        {
+            int sum = 0;
+            for(int i = startPosition; i < a.Length; i++)
+            {
+                sum += a[i];
+            }
+            return sum;
+        }
+        static int findEqualMaxTotal(int[] h1, int[] h2, int[] h3)
+        {
+            // your code goes here
+            int h1Sum = sumArray(h1, 0);
+            int h2Sum = sumArray(h2, 0);
+            int h3Sum = sumArray(h3, 0);
+            int min = h1Sum;
+            if (h1Sum == h2Sum && h1Sum == h3Sum)
+            {
+                min = h1Sum;
+                return min;
+            }
+               
+            if (h2Sum < min)
+            {
+                min = h2Sum;
+                int h2k = 0;
+                while(h2k < h2.Length)
+                {
+                    int m1 = 1;
+                    int sumh1 = 0;
+                    while(m1 < h1.Length)
+                    {
+                        sumh1 = sumArray(h1, m1);
+                        if (sumh1 <= min)
+                        {
+                            min = sumh1;
+                            break;
+                        }   
+                        m1++;
+                    }
+                    int m2 = 1;
+                    int sumh3 = 0;
+                    while (m2 < h3.Length)
+                    {
+                        sumh3 = sumArray(h3, m2);
+                        if (sumh3 <= min)
+                        {
+                            min = sumh1;
+                            break;
+                        }
+                        m2++;
+                    }
+                    if (min == sumArray(h2, h2k))
+                        return min;
+                    h2k++;
+                }
+            }
+
+            if (h3Sum < min)
+            {
+                min = h3Sum;
+                int h3k = 0;
+                while (h3k < h2.Length)
+                {
+                    int m1 = 1;
+                    int sumh1 = 0;
+                    while (m1 < h1.Length)
+                    {
+                        sumh1 = sumArray(h1, m1);
+                        if (sumh1 <= min)
+                        {
+                            min = sumh1;
+                            break;
+                        }
+                        m1++;
+                    }
+                    int m2 = 1;
+                    int sumh3 = 0;
+                    while (m2 < h3.Length)
+                    {
+                        sumh3 = sumArray(h3, m2);
+                        if (sumh3 <= min)
+                        {
+                            min = sumh1;
+                            break;
+                        }
+                        m2++;
+                    }
+                    if (min == sumArray(h2, h3k))
+                        return min;
+                    h3k++;
+                }
+            }
+
+            int h1k = 0;
+            while (h1k < h2.Length)
+            {
+                int m1 = 1;
+                int sumh1 = 0;
+                while (m1 < h1.Length)
+                {
+                    sumh1 = sumArray(h1, m1);
+                    if (sumh1 <= min)
+                    {
+                        min = sumh1;
+                        break;
+                    }
+                    m1++;
+                }
+                int m2 = 1;
+                int sumh3 = 0;
+                while (m2 < h3.Length)
+                {
+                    sumh3 = sumArray(h3, m2);
+                    if (sumh3 <= min)
+                    {
+                        min = sumh1;
+                        break;
+                    }
+                    m2++;
+                }
+                if (min == sumArray(h2, h1k))
+                    return min;
+                h1k++;
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+
+            int[] h1 = Array.ConvertAll(Console.ReadLine().Split(' '), h1Temp => Convert.ToInt32(h1Temp));
+
+            int[] h2 = Array.ConvertAll(Console.ReadLine().Split(' '), h2Temp => Convert.ToInt32(h2Temp));
+
+            int[] h3 = Array.ConvertAll(Console.ReadLine().Split(' '), h3Temp => Convert.ToInt32(h3Temp));
+
+            int result = findEqualMaxTotal(h1, h2, h3);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
